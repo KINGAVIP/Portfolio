66853091a5ee73540e0ebfee3c716d1134f0487b
KINGAVIP <avipruthi8@gmail.com>
2024-02-12 13:10:23 +0530

    Setup tailwind css

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-3 bg-blue-500">
+      <div className="  text-red-500">Logo</div>
+      <div>items</div>
+    </div>
+  );
+};
+
+export default Navbar;
