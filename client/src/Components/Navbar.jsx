0f99f0d718aedd537115821fcc62b634fa361f9b
KINGAVIP <avipruthi8@gmail.com>
2024-02-13 15:26:57 +0530

    Basic Navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,25 @@
+import dp from "../assets/dp.jpeg";
 const Navbar = () => {
+  const NavbarItem = ({ name }) => {
+    return (
+      <a
+        id={name}
+        className=" no-underline p-4   hover:text-white transition-all duration-300 cursor-pointer"
+      >
+        {name}
+      </a>
+    );
+  };
   return (
-    <div className="flex justify-between p-3 bg-blue-500">
-      <div className="  text-red-500">Logo</div>
-      <div>items</div>
+    <div className="flex items-center justify-between h-[4rem]  bg-blue-500 min-h-fit px-4">
+      <div className="  text-red-500">
+        <img src={dp} alt="dp" className="flex rounded-full h-12 w-12 " />
+      </div>
+      <div>
+        {["Home", "Education", "Skills", "Projects"].map((item) => (
+          <NavbarItem key={item} name={item} />
+        ))}
+      </div>
     </div>
   );
 };
